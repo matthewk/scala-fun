e68317bb367d722adfd17c8d54e4f13862b84ab9
flavio <flavio.ricci82@gmail.com>
2017-05-29 01:33:05 +0200

    deserializing json

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,27 @@
 package main.scala
 
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+
 /**
   * Created by flavio on 28/05/2017.
   */
   object Main {
+
+  case class Person(name: String, age: Int)
+
     def main(args: Array[String]): Unit = {
-      println("Test scala")
+
+      val jsValue = parse("""{"name":"john", "age": 28}""")
+
+      // json4s requires you to have this in scope to call extract
+      implicit val formats = DefaultFormats
+
+      val p = jsValue.extract[Person]
+      // p is Person("john",28)
+
+      println(p.name)
     }
   }
 
--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -4,5 +4,7 @@ package main.scala
   * Object containing all the models
   */
 object Model {
+  case class Person2(name: String, age: Int)
   case class Track(title: String, artist: String, id: Int, rank: Int)
+
 }
