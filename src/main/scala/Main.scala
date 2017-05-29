1bfcb51a9f9c11763d4a2b8daa39cc99983a2f70
Flavio Ricci <riccif@yoox.net>
2017-05-29 15:41:58 +0200

    h2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,29 +8,30 @@ import org.json4s.jackson.JsonMethods._
 /**
   * Created by flavio on 28/05/2017.
   */
-  object Main {
+object Main {
 
-    def main(args: Array[String]): Unit = {
+  val BaseDeezerAPIURL: String = "https://api.deezer.com/track/"
 
-      val jsValue = parse("""{"name":"john", "age": 28}""")
+  def main(args: Array[String]): Unit = {
 
-      // json4s requires you to have this in scope to call extract
-      implicit val formats = DefaultFormats
+    val jsValue = parse("""{"name":"john", "age": 28}""")
 
-      val p = jsValue.extract[Person]
-      // p is Person("john",28)
+    // json4s requires you to have this in scope to call extract
+    implicit val formats = DefaultFormats
 
-      println(p.name)
+    val p = jsValue.extract[Person]
+    // p is Person("john",28)
 
-      val track = getTrackDetails("14310750")
-      println(track.title)
-      println(track.rank)
+    println(p.name)
 
-    }
+    val track = getTrackDetails("14310750")
+    println(track.title)
+    println(track.rank)
 
-  def getTrackDetails (trackId: String) : Track = {
-    val baseDeezerURL = "https://api.deezer.com/track/"
-    val apiURL = baseDeezerURL + trackId
+  }
+
+  def getTrackDetails(trackId: String): Track = {
+    val apiURL = BaseDeezerAPIURL + trackId
     var respJSON = scala.io.Source.fromURL(apiURL).mkString
     // json4s requires you to have this in scope to call extract
     implicit val formats = DefaultFormats
@@ -38,5 +39,5 @@ import org.json4s.jackson.JsonMethods._
     val track = jsonValue.extract[Track]
     return track
   }
-  }
+}
 
